0a63f6bf9bbf22e7922fae28fa55eb13f3d888db
Mwarren0 <162091481+Mwarren0@users.noreply.github.com>
2024-06-04 08:17:36 -0500

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version('cerulean')
+game('gta5')
+lua54 'on'
+
+files {
+    'data/**/carcols.meta',
+    'data/**/carvariations.meta',
+    'data/**/contentunlocks.meta',
+    'data/**/handling.meta',
+    'data/**/vehiclelayouts.meta',
+    'data/**/vehicles.meta'
+}
+
+data_file('CONTENT_UNLOCKING_META_FILE')('data/**/contentunlocks.meta')
+
+--Made by @m.warren for FiveM
